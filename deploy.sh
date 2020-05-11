b9bc291d81f949e394ef59802b6446a39049a63f
yoann <yoann@flexper.fr>
2020-05-11 03:06:50 +0200

    ADD NEW IMG

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 IMAGE='registry.gitlab.com/fruitylab/book/angular-prerender:'$(date '+%s')
 docker build -t $IMAGE .
-#docker login registry.gitlab.com -u yoanndh
+docker login registry.gitlab.com -u yoanndh
 docker push $IMAGE
